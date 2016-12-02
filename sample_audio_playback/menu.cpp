8233346539521b9880459640832f9b79ea185bb9
frizzle101101 <aaronfritz96@gmail.com>
2016-12-02 13:08:53 -0500

    Added phone book tree implment(still dev) Implmenting Console menu(still Dev)

---
+++ sample_audio_playback/menu.cpp
@@ -0,0 +1,120 @@
+/*	Damian Scarpone
+
+September 06, 2016
+
+Program: To find the sum and average of three user picked integers
+
+Revisoins:
+*/
+/*********************************Libraries*************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "menu.h"
+
+
+/*                              Main Function                                              */
+
+void helpmenu(void)
+{
+	printf("HELP MENU\n");
+	printf("help -");
+	printf(" Output a list of valid commands\n");
+	printf("compose -");
+	printf(" Compose a message\n");
+	printf("display -");
+	printf(" Display a message\n");
+	printf("playback -");
+	printf(" Playback a message\n");
+	printf("receive -");
+	printf(" Receive a message\n");
+	printf("record -");
+	printf(" Record a message\n");
+	printf("select -");
+	printf(" Select a message or device\n");
+	printf("settings -");
+	printf(" Change parameters\n");
+	printf("remove -");
+	printf(" Remove a message\n");
+
+
+}
+void helpcompose(void)
+{
+	printf("compose\n");
+}
+
+void helpdisplay(void)
+{
+	printf("display\n");
+}
+
+void helpplayback(void)
+{
+	printf("playback\n");
+}
+
+void helpreceive(void)
+{
+	printf("receive\n");
+}
+
+void helprecord(void)
+{
+	printf("record\n");
+}
+
+void helpselect(void)
+{
+	printf("select\n");
+}
+
+void helpsettings(void)
+{
+	printf("settings\n");
+}
+
+void helpremove(void)
+{
+	printf("remove\n");
+}
+void composeMsg(void)
+{
+	printf("compose\n");
+}
+
+void displayMsg(void)
+{
+	printf("display\n");
+}
+
+void playback(void)
+{
+	printf("playback\n");
+}
+
+void receive(void)
+{
+	printf("receive\n");
+}
+
+void record(void)
+{
+	printf("record\n");
+}
+
+void select(void)
+{
+	printf("select\n");
+}
+
+void settings(void)
+{
+	printf("settings\n");
+}
+
+void remove(void)
+{
+	printf("remove\n");
+}
